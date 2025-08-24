5e60ae0808b5ba837bf3d1c2270d82960370106c
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2025-08-24 14:09:49 -0500

    second commit seimalsa2025_3

--- src/controllers/historial.controller.js
+++ src/controllers/historial.controller.js
@@ -442,15 +442,3 @@ export const getHistReparados = async (req, res) => {
         res.send(error.message);
       }
     };
-  
-    export const getReporteMentenimiento = async (req, res) => {
-      try {
-        const pool = await getConnection();
-        const result = await pool.request().
-        query(querys.getMentenimientoEquiposUrgente);
-        res.json(result.recordset);
-      } catch (error) {
-        res.status(500);
-        res.send(error.message);
-      }
-    };
--- src/routes/historial.router.js
+++ src/routes/historial.router.js
@@ -27,7 +27,6 @@ import {
   getHistorialEquipoReparadoFiltro,
   getHistorialEquipoDisponibleFiltro,
   getTopFiveItemsFiltro,
-  getReporteMentenimiento,
   //GLOBAL REFRIGERACION
   getTotalEquiposMapa,
   getTopFiveTecnicos,
@@ -66,7 +65,6 @@ router.get("/historialxyz/:serie", getHistTotalPorSerieInterno);
 router.get("/historialxxx/:placa", getHistTotalPorPlaca);
 router.get("/historialxyzw/:serie", getEntregadosPorSerie);
 router.get("/historialzzz/:codigo/:idCli1/:idCli2", getHistTotalCodSubCliente);
-router.get("/historial/a", getReporteMentenimiento);
 //GLOBAL REFRIGERACION
 router.get("/historialxy", getTotalEquiposMapa);
 router.get("/historialT", getTopFiveTecnicos);
