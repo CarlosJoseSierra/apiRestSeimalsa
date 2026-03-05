4a7e0ce7355f82edee65258fe12e2b6641dcfb3f
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2026-03-04 20:59:35 -0500

    appseimalsa13

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 
+require("dotenv/config");
 var _app = _interopRequireDefault(require("./app.js"));
-var _config = require("./config.js");
+var _config2 = require("./config.js");
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-_app["default"].listen(_config.PORT);
-console.log("Server on port", _config.PORT);
+_app["default"].listen(_config2.PORT);
+console.log("Server on port", _config2.PORT);
