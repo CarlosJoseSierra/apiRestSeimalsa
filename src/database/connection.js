acb4463006272febadf11364c07a9057e7c15404
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2026-03-04 20:26:54 -0500

    appseimalsa7

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,10 +2,10 @@ import sql from "mssql";
 import config from "../config";
 
 export const dbSettings = {
-  user:config.DB_USER,
-  password:config.DB_PASSWORD,
-  server: config.DB_SERVER,
-  database:config.DB_DATABASE,
+  DB_USER:config.DB_USER,
+  DB_PASSWORD:config.DB_PASSWORD,
+  DB_SERVER: config.DB_SERVER,
+  DB_DATABASE:config.DB_DATABASE,
   options: {
     encrypt: false, // for azure
     trustServerCertificate: true, // change to true for local dev / self-signed certs
