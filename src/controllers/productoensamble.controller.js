b65dd30756b7764c942a697ee6bfdfb80cb48ed9
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2026-05-14 17:27:54 -0500

    appseimalsa_2026_Mayo_16

--- src/controllers/productoensamble.controller.js
+++ src/controllers/productoensamble.controller.js
@@ -1,4 +1,7 @@
 import { getConnection, querys, sql } from "../database";
+const cloudinary = require("../libs/cloudinary");
+const path = require('path');
+const upload = require ('../libs/multer');
 
 export const getAllEnsambles = async (req, res) => {
     try {
